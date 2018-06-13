f869a29b71688d484e316953642a893018859392
Mikaela Scornaienchi <scornaim@gmail.com>
2018-06-13 15:57:05 -0400

    pushing final

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,10 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      userClick: false
+    }
+    this.redirectSite = this.redirectSite.bind(this);
+  }
+
+  redirectSite() {
+    console.log('clicked');
+    this.setState({
+      userClick: true
+    }, () => {
+      window.location.href ="https://ant.design/";
+    })
+  }
+  
   render() {
     return (
-      <div>
-        Hello
+      <div className="wrapper">
+        <div className="body-wrapper">
+          <div className="body">
+            <h1>Welcome to Ant Design!</h1>
+            <h2>We're the  design and UX experts! Let's build something beautiful together.</h2>
+            <button onClick={this.redirectSite}>Check us out 😊</button>
+          </div>
+        </div>
       </div>
     )
   }
